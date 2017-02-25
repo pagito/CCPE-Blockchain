bd3b2d611fb53adfad029e17aed0a4431dfee21a
Giorgi <giorgi.javrishvili@gmail.com>
2017-02-25 14:52:10 +0800

    Chaincode deployment 4

--- app-webservice/routes/index.js
+++ app-webservice/routes/index.js
@@ -31,9 +31,9 @@ var my_cc;
             }
         },
         chaincode:{
-            zip_url: 'https://github.com/CCPX-system/CCPX-blockchain/raw/master/GOLANG/ccpx/ccpx.zip',
+            zip_url: 'https://github.com/pagito/CCPE-Blockchain/blob/master/chaincode/ccpe_chaincode.zip',
             unzip_dir: '/',
-            git_url: 'https://github.com/CCPX-system/CCPX-blockchain/GOLANG/ccpx'
+            git_url: 'https://github.com/pagito/CCPE-Blockchain/tree/master/chaincode'
         }
     };
     
