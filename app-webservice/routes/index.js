48cf12bf8c64059ff5541a25a8c074ee34e6d82e
Giorgi Javrishvili <giorgi.javrishvili@gmail.com>
2017-04-14 20:04:14 +0800

    send tr still error 2 sdsd

--- app-webservice/routes/index.js
+++ app-webservice/routes/index.js
@@ -181,7 +181,7 @@ router.post('/sendTransaction', function(req, res, next) {
                 result_data.push({"test": "test_value"});
                 console.log("Result inside the loop: ");
                 console.log(result_data);
-                //result_data = JSON.stringify(result_data);    
+                result_data = JSON.stringify(result_data);    
 
                 if (key == k) {
                     res.json(result_data);
